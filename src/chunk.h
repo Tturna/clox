5b3a8333aca6340ea5949b8eae02789657219c29
Tturna <42873350+Tturna@users.noreply.github.com>
2025-01-17 14:49:47 +0200

    constants and line info

--- src/chunk.h
+++ src/chunk.h
@@ -1,10 +1,12 @@
 #ifndef clox_chunk_h
 #define clox_chunk_h
 
-#include "common.h"
 #include <stdint.h>
+#include "common.h"
+#include "value.h"
 
 typedef enum {
+    OP_CONSTANT,
     OP_RETURN,
 } OpCode;
 
@@ -14,10 +16,13 @@ typedef struct {
     int count;
     int capacity;
     uint8_t* code;
+    int* lines;
+    ValueArray constants;
 } Chunk;
 
 void initChunk(Chunk* chunk);
 void freeChunk(Chunk* chunk);
-void writeChunk(Chunk* chunk, uint8_t byte);
+void writeChunk(Chunk* chunk, uint8_t byte, int line);
+int addConstant(Chunk* chunk, Value value);
 
 #endif
--- src/main.c
+++ src/main.c
@@ -5,9 +5,13 @@ int main(int argc, const char* argv[]) {
     Chunk chunk;
     initChunk(&chunk);
 
+    int constantIndex = addConstant(&chunk, 1.2);
+    writeChunk(&chunk, OP_CONSTANT, 123);
+    writeChunk(&chunk, constantIndex, 123);
+
     // implicit cast because enum values are int types and the function takes
     // a type of uint8_t;
-    writeChunk(&chunk, OP_RETURN);
+    writeChunk(&chunk, OP_RETURN, 123);
 
     disassembleChunk(&chunk, "test chunk");
     freeChunk(&chunk);
