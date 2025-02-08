de8ffe68b21a5a6e7da31c34b1fcb9c77c4739d1
Tturna <42873350+Tturna@users.noreply.github.com>
2025-02-08 16:50:29 +0200

    closures

--- src/memory.c
+++ src/memory.c
@@ -18,6 +18,14 @@ void* reallocate(void* pointer, size_t oldSize, size_t newSize) {
 
 static void freeObject(Obj* object) {
     switch (object->type) {
+        case OBJ_CLOSURE: {
+            ObjClosure* closure = (ObjClosure*)object;
+            FREE_ARRAY(ObjUpvalue*, closure->upvalues, closure->upvalueCount);
+            // Don't free the function in a closure because there could be another closure
+            // that references the same function. The closure doesn't own the function.
+            FREE(ObjClosure, object);
+            break;
+        }
         case OBJ_FUNCTION: {
             ObjFunction* function = (ObjFunction*)object;
             freeChunk(&function->chunk);
@@ -33,6 +41,9 @@ static void freeObject(Obj* object) {
             FREE(ObjString, object);
             break;
         }
+        case OBJ_UPVALUE:
+            FREE(ObjUpvalue, object);
+            break;
     }
 }
 
